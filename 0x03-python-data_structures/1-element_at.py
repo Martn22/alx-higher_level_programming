d2628189b85e527a675dd256bf22a49456c8ec08
Martin Kiilu <martinkiilu001@gmail.com>
2022-12-05 09:14:57 -0800

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     if idx < 0:
-        return None
-    elif idx > (len(my_list)):
-        return None
+        return 'None'
+    elif idx > (len(my_list) - 1):
+        return 'None'
     else:
-        for i in range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+        return my_list[idx]
