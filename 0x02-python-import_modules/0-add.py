c780413a6d2c6a8f4e48082c6954a9e2bbfd7921
Martin Kiilu <martinkiilu001@gmail.com>
2022-12-06 10:42:58 -0800

    Task 0

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 from add_0 import add
-add(a=1, b=2)
+add()
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,4 +1,4 @@
-def add(a, b):
-    """print result of a + b"""
+def add(a=1, b=2):
+    """print result of a + b""" 
     print("{:d} + {:d} = {:d}".format(a, b, a+b))
 
