26f758f8d9b3e241d298cdbb7ecf63c90721b041
Martin Kiilu <martinkiilu001@gmail.com>
2022-12-06 11:05:59 -0800

    Task 0

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,4 +1,11 @@
-def add(a=1, b=2):
-    """print result of a + b""" 
-    print("{:d} + {:d} = {:d}".format(a, b, a+b))
+#!/usr/bin/python3
+def add(a, b):
+    """My addition function.
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        The return value. a + b
+    """
+    return (a+b)
 
