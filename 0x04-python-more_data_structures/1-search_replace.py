b6ed69cc0c8ac0edc26aaa40f9ed802f289b1749
Martin Kiilu <martinkiilu001@gmail.com>
2022-12-09 01:05:36 -0800

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,10 +2,8 @@
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
 
-    for i in range(len(new_list)):
-        if search in new_list:
+    if search in new_list:
+        for i in new_list:
             new_list[i] = replace
-        else:
-            new_list[i]
-
+    
     return (new_list)
