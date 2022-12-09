d4cf387d739427fa9947c0f22f46c9a72fcceccc
Martin Kiilu <martinkiilu001@gmail.com>
2022-12-09 00:01:06 -0800

    Task 0

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     """computes square value of all integer of a matrix and returns new matrix."""
-    matrix = []
     new_matrix = []
     for i in range(len(matrix)):
         new_matrix.append([])
         for j in range(len(matrix[i])):
             new_matrix[i].append(j**2)
     return new_matrix
+
