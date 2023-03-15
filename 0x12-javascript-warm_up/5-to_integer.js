50c0db380f3d96d451c3a8377627df7a23a95503
Martin Kiilu <martinkiilu001@gmail.com>
2023-03-15 15:46:44 -0700

    Task 5

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const process = require('process');
+const argz = process.argv;
+const myVar = Number(argz[2]);
+if (isNaN(myVar)) {
+  console.log('Not a number');
+} else {
+  console.log('My Number: ' + myVar);
+}
