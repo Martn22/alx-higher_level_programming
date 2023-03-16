3cc36f7a3b853c84210816fdb1e8de1e2f0dbef6
Martin Kiilu <martinkiilu001@gmail.com>
2023-03-16 09:44:50 -0700

    Task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+// Script that prints a square.
+const process = require('process');
+const argz = process.argv;
+const myVar = Number(argz[2]);
+let i, j;
+if (isNaN(myVar)) {
+  console.log('Missing size');
+} else {
+  for (i = 0; i < myVar; i++) {
+    let row = '';
+    for (j = 0; j < myVar; j++) {
+      row += 'x';
+    }
+    console.log(row);
+  }
+}
