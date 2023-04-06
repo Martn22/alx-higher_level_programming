21a32e05ab683ed3df219b8c0404c0d481bfb7c3
Martin Kiilu <martinkiilu001@gmail.com>
2023-04-06 01:15:07 -0700

    python_network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-#Script that fetches https://alx-intranet.hbtn.io/status using urllib package
+"""Script that fetches https://alx-intranet.hbtn.io/status 
+using urllib package.
+"""
 
 if __name__ == '__main__':
     import urllib.request
